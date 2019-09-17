2ce51c2174691cb0066addb77025cd97d713c77b
Jeramy Wenserit <jeramy@digidip.net>
2019-09-17 11:06:35 +0200

    Create index def in lib

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,11 @@
 export { run } from '@oclif/command';
+export { default as TapjawAdapter } from './contracts/tapjaw-adapter';
+export { default as TapjawConnector } from './contracts/tapjaw-connector';
+export { default as TapjawAuthenticator } from './contracts/tapjaw-authenticator';
+export { default as TapjawIterator } from './contracts/tapjaw-iterator';
+export { default as TapjawMessage } from './contracts/tapjaw-message';
+export { default as TapjawCommand } from './contracts/tapjaw-command';
+export { default as TapjawHttpConnector } from './connectors/tapjaw-http-connector';
+export { default as TapjawBasicAuthenticator } from './authenticators/basic-auth-authenticator';
+export { default as TapjawBearerAuthenticator } from './authenticators/bearer-auth-authenticator';
+export { default as TapjawOauthAuthenticator } from './authenticators/oauth-authenticator';
