db875c02db9adfb2840093e3bb311af98dc13abd
Jeramy Wenserit <jeramy@digidip.net>
2019-09-17 11:11:49 +0200

    fix command issue

--- lib/contracts/tapjaw-command.d.ts
+++ lib/contracts/tapjaw-command.d.ts
@@ -2,11 +2,11 @@ import { Command, flags } from '@oclif/command';
 import TapjawIterator from './tapjaw-iterator';
 import TapjawAdapter from './tapjaw-adapter';
 export default abstract class TapjawCommand extends Command {
+    static args: never[];
+    static defaultFlags: flags.Input<any>;
     abstract instance: any;
     abstract flags: flags.Input<any>;
-    static args: never[];
     protected abstract iterator: TapjawIterator;
     protected abstract adapter: TapjawAdapter;
-    protected defaultFlags: flags.Input<any>;
     run(): Promise<void>;
 }
--- src/contracts/tapjaw-command.ts
+++ src/contracts/tapjaw-command.ts
@@ -4,15 +4,16 @@ import TapjawAdapter from './tapjaw-adapter';
 import { IConfig } from '@oclif/config';
 
 export default abstract class TapjawCommand extends Command {
+    static args = [];
+    static defaultFlags: flags.Input<any> = {
+        help: flags.help({ char: 'h' }),
+    };
+
     abstract instance: any; // @todo see if we can honour Parser.Input
     abstract flags: flags.Input<any>;
-    static args = [];
 
     protected abstract iterator: TapjawIterator;
     protected abstract adapter: TapjawAdapter;
-    protected defaultFlags: flags.Input<any> = {
-        help: flags.help({ char: 'h' }),
-    };
 
     // protected setDependency(adapter: TapjawAdapter, iterator: TapjawIterator): void {
     //     this.adapter = adapter;
