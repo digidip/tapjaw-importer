030a4e31662336ffb5e3b854f630eb13746f671d
Jeramy Wenserit <jeramy@digidip.net>
2019-09-17 13:49:32 +0200

    typescript adjustment

--- lib/contracts/tapjaw-adapter.d.ts
+++ lib/contracts/tapjaw-adapter.d.ts
@@ -1,5 +1,7 @@
 import TapjawMessage from './tapjaw-message';
 export declare class TapjawAdapterError extends Error {
 }
-declare type TapjawAdapter<T extends TapjawMessage> = () => AsyncGenerator<T>;
+interface TapjawAdapter<T extends TapjawMessage> {
+    (): AsyncGenerator<T>;
+}
 export default TapjawAdapter;
--- src/contracts/tapjaw-adapter.ts
+++ src/contracts/tapjaw-adapter.ts
@@ -2,6 +2,8 @@ import TapjawMessage from './tapjaw-message';
 
 export class TapjawAdapterError extends Error {}
 
-type TapjawAdapter<T extends TapjawMessage> = () => AsyncGenerator<T>;
+interface TapjawAdapter<T extends TapjawMessage> {
+    (): AsyncGenerator<T>;
+}
 
 export default TapjawAdapter;
