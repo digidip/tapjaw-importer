dea6dfe36cb68f21f40718a6a191bcfc74dde7d3
Jeramy Wenserit <jeramyw@xylesoft.co.uk>
2019-11-29 14:53:41 +0100

    Add security helper methods

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+
+import * as querystring from 'querystring';
+
 export { run } from '@oclif/command';
 
 // Contracts
@@ -13,12 +16,54 @@ export { default as TapjawHttpConnector } from './connectors/tapjaw-http-connect
 // export { default as TapjawSoapConnector } from './connectors/tapjaw-soap-connector';
 
 // Authenticators
-export { default as TapjawBasicAuthenticator } from './authenticators/basic-auth-authenticator';
-export { default as TapjawBearerAuthenticator } from './authenticators/bearer-auth-authenticator';
-export { default as TapjawOauthAuthenticator } from './authenticators/oauth-authenticator';
+import { default as TapjawBasicAuthenticator } from './authenticators/basic-auth-authenticator';
+import { default as TapjawBearerAuthenticator } from './authenticators/bearer-auth-authenticator';
+import { default as TapjawOauthAuthenticator } from './authenticators/oauth-authenticator';
 
-export { default as TapjawApplyAuthorizationHttpHeaderWrapper } from './authenticators/wrappers/apply-authorization-http-header-wrapper';
-export { default as TapjawApplyOauthAuthorizationHttpHeaderWrapper } from './authenticators/wrappers/apply-oauth-authorization-http-header-wrapper';
+// @deprecate in 0.2.0 release.
+export {
+    TapjawBasicAuthenticator,
+    TapjawBearerAuthenticator,
+    TapjawOauthAuthenticator
+};
+
+import { default as TapjawApplyAuthorizationHttpHeaderWrapper } from './authenticators/wrappers/apply-authorization-http-header-wrapper';
+import { default as TapjawApplyOauthAuthorizationHttpHeaderWrapper } from './authenticators/wrappers/apply-oauth-authorization-http-header-wrapper';
+
+// @deprecate in 0.2.0 release.
+export {
+    TapjawApplyAuthorizationHttpHeaderWrapper,
+    TapjawApplyOauthAuthorizationHttpHeaderWrapper
+};
+
+const createBasicSecurity = (username: string, password: string) => new TapjawApplyAuthorizationHttpHeaderWrapper(new TapjawBasicAuthenticator(username, password));
+const createBearerSecurity = (token: string) => new TapjawApplyAuthorizationHttpHeaderWrapper(new TapjawBearerAuthenticator(token));
+const createOAuthSecurity = (
+    clientId: string,
+    clientSecret: string,
+    hostname: string,
+    path: string,
+    postParams: querystring.ParsedUrlQueryInput,
+    method: string = 'POST',
+    responseEncoding = 'utf8',
+) => new TapjawApplyOauthAuthorizationHttpHeaderWrapper(
+        new TapjawOauthAuthenticator(
+            clientId,
+            clientSecret,
+            hostname,
+            path,
+            postParams,
+            method,
+            responseEncoding
+        )
+    );
+
+
+export {
+    createBasicSecurity,
+    createBearerSecurity,
+    createOAuthSecurity,
+}
 
 // Iterators
 export { default as StdoutIterator } from './iterators/stdout-iterator';
@@ -26,3 +71,7 @@ export { default as OutputIterator } from './iterators/output-iterator';
 
 // Support
 export { default as sortObjectArrays } from './support/sort-object-arrays';
+
+// Parsers
+export { default as xmlToJson } from './parsers/xml-to-json';
+export { default as csvToJson } from './parsers/csv-to-json';
