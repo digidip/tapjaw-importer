b7e57c263dcdb84d8c962a7a71a80933e1be6720
Jeramy Wenserit <jeramy@digidip.net>
2021-09-01 16:02:33 +0200

    Add tapjaw payload to index

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,7 +2,7 @@ export { default as TapjawAdapter } from './contracts/tapjaw-adapter';
 export { default as TapjawConnector } from './contracts/tapjaw-connector';
 export { default as TapjawAuthenticator } from './contracts/tapjaw-authenticator';
 export { default as TapjawIterator } from './contracts/tapjaw-iterator';
-export { default as TapjawMessage } from './contracts/tapjaw-message';
+export { default as TapjawMessage, TapjawPayload } from './contracts/tapjaw-message';
 export { default as TapjawCommand, TapjawCommandDefaultFlags, TapjawCommandArgs, TapjawCommandFlags, } from './contracts/tapjaw-command';
 export { default as TapjawHttpConnector } from './connectors/tapjaw-http-connector';
 export { default as TapjawHtmlFormExtractor, InputField, Form } from './authenticators/support/html-form-extractor';
