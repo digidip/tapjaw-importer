db4dc7d86286b3e89c95bccebccef8120eccf416
Jeramy Wenserit <jeramy@digidip.net>
2021-11-04 11:38:39 +0100

    export base command defaults

--- lib/contracts/commands/index.d.ts
+++ lib/contracts/commands/index.d.ts
@@ -1,5 +1,6 @@
 export * from './base-tapjaw-command';
-export * from './tapjaw-api-command';
-export * from './tapjaw-filter-command';
-export * from './tapjaw-store-command';
-export * from './tapjaw-tool-command';
+export { default as BaseTapjawCommand } from './base-tapjaw-command';
+export { default as TapjawApiCommand } from './tapjaw-api-command';
+export { default as TapjawFilterCommand } from './tapjaw-filter-command';
+export { default as TapjawStoreCommand } from './tapjaw-store-command';
+export { default as TapjawToolCommand } from './tapjaw-tool-command';
