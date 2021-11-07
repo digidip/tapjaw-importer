29d69e0354ca84bdbbd75b7d53f9ebb391ea7fb1
Jeramy Wenserit <jeramy@digidip.net>
2021-11-07 17:03:47 +0100

    Fix missing abstract properties on TapjawDefaultConnector

--- lib/connectors/tapjaw-default-connector.d.ts
+++ lib/connectors/tapjaw-default-connector.d.ts
@@ -3,5 +3,8 @@ import { TapjawHttpConnector } from '..';
  * Reflection based configurable HTTP and HTTPS API request wrapper.
  */
 export default abstract class TapjawDefaultConnector extends TapjawHttpConnector {
+    enableGzip: boolean;
+    useDecoding: undefined;
+    useEncoding: undefined;
     constructor();
 }
--- src/connectors/tapjaw-default-connector.ts
+++ src/connectors/tapjaw-default-connector.ts
@@ -5,6 +5,10 @@ import connectorRegister from '../reflection/connector-register';
  * Reflection based configurable HTTP and HTTPS API request wrapper.
  */
 export default abstract class TapjawDefaultConnector extends TapjawHttpConnector {
+    public enableGzip = false;
+    public useDecoding = void 0;
+    public useEncoding = void 0;
+
     public constructor() {
         super('initialising');
 
