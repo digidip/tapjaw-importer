a9620c7387d35e83de1beb6781230bd5c2aa90e5
Jeramy Wenserit <jeramy@digidip.net>
2021-11-10 20:29:44 +0100

    Fix naming and improve api docs

--- lib/messages/index.d.ts
+++ lib/messages/index.d.ts
@@ -1,3 +1,3 @@
 export { default as DefaultMessage } from './tapjaw-message';
 export * from './tapjaw-message';
-export { default as sortObjevtArrays } from '../support/sort-object-arrays';
+export { default as sortObjectArrays } from '../support/sort-object-arrays';
